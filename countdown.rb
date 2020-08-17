97771ab4d888b63b2feac2e331226956a33d7273
Richard Kurwitz <kurwitz3@aol.com>
2020-08-17 23:10:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,8 +6,8 @@ def countdown(int)
 end
 end
 def countdown_with_sleep(int1)
-  while int > 0 
-  puts "#{int} SECOND(S)!"
+  while int1 > 0 
+  puts "#{int1} SECOND(S)!"
   sleep 1 
   int -= 1 
 end
